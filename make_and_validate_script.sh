eb7781e90574c912b01e6143d19a39aea7f0a1a8
b-long <b-long@users.noreply.github.com>
2024-11-21 22:45:41 -0500

    Version 0.1.8: updates to infrastructure (cibuildwheel, packaging, wheel, & gopy) (#20)
    Version 0.1.8: updates to infrastructure (cibuildwheel, packaging, wheel, & gopy)

--- make_and_validate_script.sh
+++ make_and_validate_script.sh
@@ -31,7 +31,7 @@ source "$( poetry env info --path )/bin/activate"
 
 python3 -m pip install pybindgen
 go install golang.org/x/tools/cmd/goimports@latest
-go install github.com/go-python/gopy@v0.4.9
+go install github.com/go-python/gopy@v0.4.10
 
 # For every step below, 'which python' should return '.venv/bin/python'
 PATH="$PATH:$HOME/go/bin" gopy build --output=otdf_python -vm=python3 .
@@ -42,7 +42,7 @@ python3 -m pip install --upgrade setuptools wheel
 python3 setup.py bdist_wheel
 
 # Prove that the wheel can be installed
-pip install dist/otdf_python-0.1.1-py3-none-any.whl
+pip install dist/otdf_python-0.1.8-py3-none-any.whl
 
 if [[ "$SKIP_TESTS" == "-s" || "$SKIP_TESTS" == "--skip-tests" ]]; then
     echo "Build is complete, skipping tests."
