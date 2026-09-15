09d6ae7eda707e6439cc5eb83f049dc503456d51
b-long <b-long@users.noreply.github.com>
2026-09-14 22:52:23 -0400

    fix ruff formatting

--- packages/otdf-python/src/otdf_python/cli.py
+++ packages/otdf-python/src/otdf_python/cli.py
@@ -325,9 +325,7 @@ def cmd_encrypt(args):
             logger.debug("Creating TDF")
             config = create_tdf_config(sdk, args)
             output_stream = BytesIO()
-            _manifest, size, _ = sdk.create_tdf(
-                BytesIO(payload), config, output_stream
-            )
+            _manifest, size, _ = sdk.create_tdf(BytesIO(payload), config, output_stream)
             output_file.write(output_stream.getvalue())
             logger.info(f"Created TDF of size {size} bytes")
 
